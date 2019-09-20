8d0999b8f4533208127ab4706e85fe6a7e789682
Benjamin Baum <bjb253@cornell.edu>
2019-09-20 13:08:55 -0400

    Bag

--- docs/assets/js/search.js
+++ docs/assets/js/search.js
@@ -126,6 +126,22 @@ typedoc.search.data = {
       url: "modules/_generateexpression_.html#generateexpression",
       classes: "tsd-kind-function tsd-parent-kind-external-module",
       parent: '"generateExpression"'
+    },
+    {
+      id: 15,
+      kind: 1,
+      name: '"__tests__/generateBag-tests"',
+      url: "modules/___tests___generatebag_tests_.html",
+      classes: "tsd-kind-external-module"
+    },
+    {
+      id: 16,
+      kind: 64,
+      name: "bagCanEvaluateToValue",
+      url: "modules/___tests___generatebag_tests_.html#bagcanevaluatetovalue",
+      classes:
+        "tsd-kind-function tsd-parent-kind-external-module tsd-is-not-exported",
+      parent: '"__tests__/generateBag-tests"'
     }
   ]
 };
